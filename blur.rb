910843df9127e6d5ccda56acd48e870ed6a3ca5e
Jackson Scott <scottja23@gmail.com>
2019-07-14 22:15:48 +0000

    loop error

--- blur.rb
+++ blur.rb
@@ -1,6 +1,6 @@
 class Image
-    def initialize (image_array)
-        @image = image_array
+    def initialize (image)
+        @image = image
     end
     
     def output_image
@@ -8,7 +8,31 @@ class Image
             puts arrays.join
         end
     end
+
+    def blur
+
+      @image.each do |blur|
+        case 
+        #assuming image will be in 4 rows and 4 columns 
+        #first row
+        when @image[0][0] == 0 && @image[0][1] == 1 || @image[1][0] == 1 then @image[0][0] = 1
+        when @image[0][1] == 0 && @image[0][0] == 1 || @image[0][2] == 1 || @image[1][1] == 1 then @image[0][1] = 1
+        when @image[0][2] == 0 && @image[0][1] == 1 || @image[0][3] == 1 || @image[1][2] == 1 then @image[0][2] = 1 
+        when @image[0][3] == 0 && @image[0][2] == 1 || @image[1][3] == 1 then @image[0][3] = 1
+
+        #second row
+        when @image[1][0] == 0 && @image[0][0] == 1 || @image[1][1] == 1 || @image[2][0] == 1 then @image[1][0] = 1
+        when @image[1][1] == 0 && @image[0][1] == 1 || @image[1][0] == 1 || @image[1][2] == 1 || @image[2][1] == 1 then @image[1][1] = 1
+        when @image[1][2] == 0 && @image[0][2] == 1 || @image[1][1] == 1 || @image[1][3] == 1 || @image[2][2] == 1 then @image[1][2] = 1
+        when @image[1][3] == 0 && @image[0][3] == 1 || @image[1][2] == 1 || @image[2][3] == 1 then @image[1][3] = 1
+
+        end
+        puts blur.join
+      end
+    end
+        
 end
+
 
 
 image = Image.new([
@@ -17,4 +41,4 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
-image.output_image
+image.blur 
